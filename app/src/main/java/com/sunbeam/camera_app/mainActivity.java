eaa06fd50ad0c1e14fa27b9ede0f15fb3f8dc6cb
mahima2309 <mkanpate17@gmail.com>
2025-09-09 16:25:47 +0530

    image save to the gallery

--- app/src/main/java/com/sunbeam/camera_app/mainActivity.java
+++ app/src/main/java/com/sunbeam/camera_app/mainActivity.java
@@ -4,7 +4,10 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +20,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class mainActivity extends AppCompatActivity {
 
@@ -57,17 +64,51 @@ public class mainActivity extends AppCompatActivity {
         }
 
         protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+            super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == REQUEST_IMAGECAPTURE && resultCode == RESULT_OK) {
-            //Ggetting the capture image as bitmap
+            if (requestCode == REQUEST_IMAGECAPTURE && resultCode == RESULT_OK) {
+                //getting the capture image as bitmap
 
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
 
-            image_view.setImageBitmap(imageBitmap);
+                image_view.setImageBitmap(imageBitmap);
+
+                saveImageToGallary(imageBitmap);
+            }
         }
+        private void saveImageToGallary(Bitmap imageBitmap) {
+        File storage = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+            // Create new file name for saved image
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault())
+                    .format(new Date());
+            String timestamp = "";
+            String fileName = "IMG_" + timestamp +".jpg";
+
+            File imageFile = new File(storage, fileName);
+
+            try{
+
+                // Write image bitmap to file
+                FileOutputStream outputStream = new FileOutputStream(imageFile);
+                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                outputStream.flush();
+                outputStream.close();
+
+                // Add image to gallery
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                mediaScanIntent.setData(Uri.fromFile(imageFile));
+                sendBroadcast(mediaScanIntent);
+
+                Toast.makeText(this, "Image save Successfully", Toast.LENGTH_SHORT).show();
+
+            } catch(Exception e) {
+
+            }
         }
+
+
 
 
     }
