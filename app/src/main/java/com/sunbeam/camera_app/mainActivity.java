efca65aced06971e60df6d470472573a82b9b285
mahima2309 <mkanpate17@gmail.com>
2025-09-09 15:29:53 +0530

    image uniformly to fit , Adjusts ImageView, adding permission in mainActivity file

--- app/src/main/java/com/sunbeam/camera_app/mainActivity.java
+++ app/src/main/java/com/sunbeam/camera_app/mainActivity.java
@@ -5,9 +5,11 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
@@ -15,7 +17,8 @@ import java.util.List;
 
 public class mainActivity extends AppCompatActivity {
 
-    private static final int PERMISSION_REQUEST_CODE = 123;
+    private static final int REQUEST_CAMERA_PERMISSION_CODE = 1;
+    private static final int REQUEST_IMAGECAPTURE=2;
 
     Button switchbtn;
 
@@ -25,31 +28,6 @@ public class mainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        switchbtn = findViewById(R.id.switchbtn);
-        switchbtn.setOnClickListener(v -> requestPermissions());
 
     }
-
-    private void requestPermissions() {
-
-        Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.CAMERA,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-
-    };
-
-    List<String> permissionRequest = new ArrayList<>();
-
-     for (String permission : permissions) {
-        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-            permissionRequest.add(permission);
-        }
-    }
-
-}
-
-
-
 }
