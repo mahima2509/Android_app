7f97d8180765e3621d1dbdd91357cb8fe0931445
mahima2309 <mkanpate17@gmail.com>
2025-09-09 15:55:22 +0530

    check camera permission granted, and open camera functionality added

--- app/src/main/java/com/sunbeam/camera_app/mainActivity.java
+++ app/src/main/java/com/sunbeam/camera_app/mainActivity.java
@@ -1,10 +1,13 @@
 package com.sunbeam.camera_app;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -20,7 +23,7 @@ public class mainActivity extends AppCompatActivity {
     private static final int REQUEST_CAMERA_PERMISSION_CODE = 1;
     private static final int REQUEST_IMAGECAPTURE=2;
 
-    Button switchbtn;
+    ImageView image_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,28 @@ public class mainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        image_view = findViewById(R.id.image_view);
+
 
     }
-}
+
+
+    public void captureImage(View view){
+        // Check if camera permission is granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        != PackageManager.PERMISSION_GRANTED) {
+            //Request camera permission if not granted
+           ActivityCompat.requestPermissions(this,
+                   new String[]{Manifest.permission.CAMERA},
+                   REQUEST_CAMERA_PERMISSION_CODE);
+            return;
+           }
+
+        //open the camera
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,REQUEST_IMAGECAPTURE);
+        }
+
+
+    }
